abf50a09fe9f92ca4d34559d9f286d835b04e832
Nirmalie <wnirmalie@yahoo.com>
2020-06-29 16:42:42 +0200

    Updates

---
+++ precache-manifest.f5c7cab6a73e6f0858f3476b1273df3f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bd449f634c57f04e9913d388ea33ec8",
+    "url": "/reactapp/index.html"
+  },
+  {
+    "revision": "23ae0549918422ba687d",
+    "url": "/reactapp/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "fe26edd4d2e848f68c1e",
+    "url": "/reactapp/static/css/main.891d0d62.chunk.css"
+  },
+  {
+    "revision": "23ae0549918422ba687d",
+    "url": "/reactapp/static/js/2.a8e35c0a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactapp/static/js/2.a8e35c0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe26edd4d2e848f68c1e",
+    "url": "/reactapp/static/js/main.cb4b3204.chunk.js"
+  },
+  {
+    "revision": "e219a2030a13ac34f0be",
+    "url": "/reactapp/static/js/runtime-main.f935dd26.js"
+  }
+]);
